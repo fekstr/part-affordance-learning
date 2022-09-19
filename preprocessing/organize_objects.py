0e348c86de164861c25c18a3e727f8dda8e92eea
Frithiof Ekström <frithiof.ekstrom@gmail.com>
2022-09-19 20:37:32 +0200

    Extend organization script to divide data into train and test directories

--- preprocessing/organize_objects.py
+++ preprocessing/organize_objects.py
@@ -2,6 +2,7 @@ import os
 import pickle
 from pathlib import Path
 from tqdm import tqdm
+from preprocessing.utils import load_split
 
 with open('./data/object_ids.pkl', 'rb') as f:
     obj_id_map = pickle.load(f)
@@ -20,3 +21,32 @@ for id in tqdm(all_ids):
         os.rename(f'{data_path}/{id}', f'{selected_path}/{id}')
 
 os.rename(data_path, './data/PartNet/other_objects')
+
+# Organize in train and test folders
+selected_path = './data/PartNet/selected_objects'
+
+train_objects, test_objects, _ = load_split()
+train_ids = []
+test_ids = []
+for obj, ids in obj_id_map.items():
+    if obj in train_objects:
+        train_ids += ids
+    elif obj in test_objects:
+        test_ids += ids
+    else:
+        raise Exception('Unknown object', obj)
+
+selected_dirs = tuple(os.listdir(selected_path))
+
+def move_ids(src, dest, ids):
+    Path(dest).mkdir(parents=True, exist_ok=True) 
+    for id in tqdm(ids):
+        if id in selected_dirs:
+            os.rename(
+                os.path.join(selected_path, id),
+                os.path.join(dest, id)
+            )
+
+
+move_ids(selected_path, os.path.join(selected_path, 'train'), train_ids)
+move_ids(selected_path, os.path.join(selected_path, 'test'), test_ids)
